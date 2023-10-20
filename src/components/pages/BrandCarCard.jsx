70f51c092fe383a796e011e37096cdce44e68832
rakiburrahman307 <rakiburrahman307@gmail.com>
2023-10-20 20:27:44 +0600

    add the advertisement in the brand section

--- src/components/pages/BrandCarCard.jsx
+++ src/components/pages/BrandCarCard.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 
+
 const BrandCarCard = ({ car }) => {
     const { _id, name, brand, price, imageUrl, description, rating } = car;
 
@@ -22,25 +23,31 @@ const BrandCarCard = ({ car }) => {
         ));
     };
 
-    return (
-        <div className="card card-compact w-auto bg-base-100 shadow-xl">
-            <figure><img className='h-48' src={imageUrl} alt={name} /></figure>
-            <div className="card-body">
-                <h2 className="card-title font-bold">Name: {name}</h2>
-                <h3 className="text-black text-xl font-semibold">Brand: {brand}</h3>
-                <h3 className="text-black text-sm font-semibold">Price: ${price}</h3>
-                <p>{description.length > 30 ? `${description.substring(0, 60)}...` : description}</p>
-                <div className="rating mb-6">
-                    {renderStars()}
-                </div>
 
-                <div className="card-actions justify-between">
-                    <Link to={`/details/${_id}`}><button className="btn btn-outline btn-info">
-                        Details
-                    </button></Link>
-                    <Link to={`/update/${_id}`}> <button className="btn btn-outline btn-warning">
-                        Update
-                    </button></Link>
+    return (
+        <div>
+
+            <div>
+                <div className="card card-compact w-auto bg-base-100 shadow-xl">
+                    <figure><img className='h-48' src={imageUrl} alt={name} /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title font-bold">Name: {name}</h2>
+                        <h3 className="text-black text-xl font-semibold">Brand: {brand}</h3>
+                        <h3 className="text-black text-sm font-semibold">Price: ${price}</h3>
+                        <p>{description.length > 30 ? `${description.substring(0, 60)}...` : description}</p>
+                        <div className="rating mb-6">
+                            {renderStars()}
+                        </div>
+
+                        <div className="card-actions justify-between">
+                            <Link to={`/details/${_id}`}><button className="btn btn-outline btn-info">
+                                Details
+                            </button></Link>
+                            <Link to={`/update/${_id}`}> <button className="btn btn-outline btn-warning">
+                                Update
+                            </button></Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/pages/BrandCars.jsx
+++ src/components/pages/BrandCars.jsx
@@ -1,17 +1,73 @@
 
 import { useLoaderData, useParams } from "react-router-dom";
 import BrandCarCard from "./BrandCarCard";
+import { useRef } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import './styles.css';
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
 
 const BrandCars = () => {
     const allProducts = useLoaderData();
     const { brandName } = useParams();
     const filteredProducts = allProducts.filter(product => product.brand.toLowerCase() === brandName.toLocaleLowerCase());
 
+
+    const progressCircle = useRef(null);
+    const progressContent = useRef(null);
+    const onAutoplayTimeLeft = (s, time, progress) => {
+        progressCircle.current.style.setProperty('--progress', 1 - progress);
+        progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+    };
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 my-10 mx-2 px-6">
-            {
-                filteredProducts.map(car => <BrandCarCard key={car._id} car={car}></BrandCarCard>)
-            }
+        <div>
+            <div className='mb-10'>
+                <Swiper
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Autoplay, Pagination, Navigation]}
+                    onAutoplayTimeLeft={onAutoplayTimeLeft}
+                    className="mySwiper"
+                >
+                    <SwiperSlide><img src="https://i.ibb.co/R9Sc45n/matthew-sichkaruk-QUHRL5-Z-1e-A-unsplash.jpg" /></SwiperSlide>
+                    <SwiperSlide><img src="https://i.ibb.co/jg5XW8K/akram-huseyn-l-LOJ-DA54h0-unsplash.jpg" /></SwiperSlide>
+                    <SwiperSlide><img src="https://i.ibb.co/S62YGQ9/velito-xd-I8d-Bx1-SA8-unsplash.jpg" /></SwiperSlide>
+                    <SwiperSlide><img src="https://i.ibb.co/WvLNjVt/pierre-gui-3-IR3fozcc-Y8-unsplash.jpg" /></SwiperSlide>
+                    <SwiperSlide><img src="https://i.ibb.co/TY07rGb/rodan-can-6cq-JPe-TIuls-unsplash.jpg" /></SwiperSlide>
+                    <SwiperSlide><img src="https://i.ibb.co/bjhC6Qm/jakob-rosen-W7-HOXq-Ek-JUw-unsplash.jpg" /></SwiperSlide>
+                    <SwiperSlide><img src="https://i.ibb.co/LJNMsp6/tesson-thaliath-1-Ms-GQCOe-FRk-unsplash.jpg" /></SwiperSlide>
+
+                    <div className="autoplay-progress" slot="container-end">
+                        <svg viewBox="0 0 48 48" ref={progressCircle}>
+                            <circle cx="24" cy="24" r="20"></circle>
+                        </svg>
+                        <span ref={progressContent}></span>
+                    </div>
+                </Swiper>
+            </div>
+            <div>
+                <h2 className="text-center text-3xl font-bold">All <span className="text-green-500">Cars</span></h2>
+                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 my-10 mx-2 px-6">
+                    {
+                        filteredProducts.map(car => <BrandCarCard key={car._id} car={car}></BrandCarCard>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
